2748f8cf90c6cc4024f22676ad9706b17b1c8f5f
Snitchfler <aropergaming@gmail.com>
2022-01-25 12:25:54 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Dunia!");
+        Scanner s = new Scanner(System.in);
+        String nama;
+        System.out.print("Nama : ");
+        nama = s.nextLine();
+        System.out.println("Hallo " + nama);
+    
+    }
+}
